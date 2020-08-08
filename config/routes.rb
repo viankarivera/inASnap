fad85c3667de97f3310a077253774d138a04fa4e
viankarivera <viankalrivera@gmail.com>
2020-08-08 16:36:44 -0400

    add photographer show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     get '/signup', to: 'users#new'
     post '/signup', to: 'users#create'
 
+    get '/login', to: 'sessions#new'
+
     resources :photographers
 end
