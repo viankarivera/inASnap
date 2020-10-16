b3a764d51181ca0ebdb433b613f3f9541481367d
viankarivera <viankalrivera@gmail.com>
2020-10-16 14:30:11 -0400

    commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,20 +7,25 @@ class SessionsController < ApplicationController
     end 
 
     def create #omniauth log in
+        #byebug
         if auth_hash = request.env['omniauth.auth']
             
-           oauth_email = request.env['omniauth.auth']['email']
+           oauth_email = request.env['omniauth.auth']['info']['email']
             if @user = User.find_by(:email => oauth_email)
                 session[:user_id] = @user.id
+
+               # redirect_to photographers_path
             else
+              # byebug
                 @user = User.create(:email => oauth_email, :password => SecureRandom.hex)
                 @user.save
                 session[:user_id] = @user.id
-
-               # redirect_to photographers_path(@user)
+               # byebug
+               # redirect_to photographers_path
             end 
 
         else #regular log in
+          # byebug
             @user = User.find_by_username(params[:username])
                 if @user == nil || @user.authenticate(params[:password]) == false
                 flash[:message] = "User not found."
